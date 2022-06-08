1311e77ad50bfa9a9763cfe0ccee7813f87a557b
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-08 05:58:45 +0300

    add media url and root config and added it to TEMPLATES in settings.py; add static route that references to the location of new files in instergram/urls.py

--- instergram/urls.py
+++ instergram/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from .views import *
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls import static
 
 urlpatterns= [
     path('',views.homepage,name='homepage'),
@@ -9,3 +11,5 @@ urlpatterns= [
     path('login/', auth_views.LoginView.as_view(),name='login'),
     path('logout/',auth_views.LoginView.as_view(),name='logout'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- thedrop/settings.py
+++ thedrop/settings.py
@@ -69,6 +69,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -128,8 +129,11 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
