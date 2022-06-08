7df2bd102ea7672bc32d4fc93a93bafb7bbe62a2
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-08 08:17:46 +0300

    register models in admin.py; create newcommentform

--- instergram/admin.py
+++ instergram/admin.py
@@ -4,3 +4,6 @@ from .models import *
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Likes)
+admin.site.register(Follow)
--- instergram/forms.py
+++ instergram/forms.py
@@ -1,9 +1,21 @@
 from dataclasses import field
-from django.forms import forms
+from django.forms import forms,ModelForm,TextInput
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from django import forms
 
-
-class UserSignUpForm(forms.Form):
+class UserSignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','password']
+        
+
+class NewCommentForm(forms.ModelForm):
+    comment = forms.CharField(max_length=3300, required=True)
+    class Meta:
+        model = Comment
+        fields = ["comment",]
+        widgets = {
+            'comment': TextInput(attrs={'class': 'input', 'placeholder': 'Write comment','style':'max-width:100%'})
+        }
