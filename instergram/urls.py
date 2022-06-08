155ae73ac05ff8b79ab5732953d2e89336212286
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-08 06:42:43 +0300

    install whitenoise and configure it in settings line 132;add all-pages folder with files; add static folder

--- instergram/urls.py
+++ instergram/urls.py
@@ -12,5 +12,5 @@ urlpatterns= [
     path('logout/',auth_views.LoginView.as_view(),name='logout'),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- instergram/views.py
+++ instergram/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import *
 
 # Create your views here.
-@login_required(login_url='/accounts/login/')
+@login_required(login_url='login')
 def homepage(request):
-    return HttpResponse("Hello World")
+    user= request.user
+    all_users = User.objects.all()
+    profile = Profile.objects.all()
+    post = Post.objects.all()
+    title = 'Welcome to instagram'
+        
+    context = {
+        'post': post,
+        'profile': profile,
+        'all_users': all_users,
+        'title':title
+    }
+    return render(request,'all-pages/homepage.html', context=context)
 def signup(request):
     pass
 def signin(request):
