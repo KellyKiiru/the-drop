cdfa283354dfb6bccf443d06c954ca5d2bd3d04e
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-08 06:08:45 +0300

    patch: static module import error online 6 of instergram/urls.py

--- instergram/urls.py
+++ instergram/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import *
 from django.contrib.auth import views as auth_views
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 urlpatterns= [
     path('',views.homepage,name='homepage'),
@@ -12,4 +12,5 @@ urlpatterns= [
     path('logout/',auth_views.LoginView.as_view(),name='logout'),
 ]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
