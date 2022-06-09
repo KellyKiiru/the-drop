9fe87c7df750614dec9dd4f2fd8be741f6165d68
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-10 00:01:29 +0300

    comment out login and logout paths in project paths

--- thedrop/urls.py
+++ thedrop/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('instergram.urls')),
     path('accounts/',include('registration.backends.simple.urls')),
-    path('login/', auth_views.LoginView.as_view(),name='login'),
-    path('logout/', auth_views.LogoutView.as_view(),name='logout'),
+    #path('login/', auth_views.LoginView.as_view(),name='login'),
+    #path('logout/', auth_views.LogoutView.as_view(),name='logout'),
 ]
