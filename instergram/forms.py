11945d1cc132af839772194fa885787c4af65798
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-06-10 00:25:07 +0300

    create user input fields for EditProfileForm

--- instergram/forms.py
+++ instergram/forms.py
@@ -32,6 +32,15 @@ class NewPostform(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['picture', 'caption']
-class EditProfile(forms.ModelForm):
-    class meta:
-        pass
+        
+class EditProfileForm(forms.ModelForm):
+    image = forms.ImageField(required=True)
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'First Name'}), required=True)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Last Name'}), required=True)
+    bio = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Bio'}), required=True)
+    url = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'URL'}), required=True)
+    location = forms.CharField(widget=forms.TextInput(attrs={'class': 'input', 'placeholder': 'Address'}), required=True)
+
+    class Meta:
+        model = Profile
+        fields = ['image', 'first_name', 'last_name', 'bio', 'url', 'location']
