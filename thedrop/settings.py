770f326426173ebb378988fdcaf8664903bf3ac9
KellyKiiru <infowithkiiru@gmail.com>
2022-09-06 02:55:17 +0300

    install django-allauth; add the associated apps to the installed apps dictionary

--- thedrop/settings.py
+++ thedrop/settings.py
@@ -45,6 +45,15 @@ INSTALLED_APPS = [
     'instergram',
     'bootstrap3',
     'tinymce',
+
+    # The following apps are required:
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -138,3 +147,19 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
+
+SITE_ID = 1 
+
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'https'
