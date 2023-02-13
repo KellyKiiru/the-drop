6228f0ae57f5e244626c66fdbe5b910406a26d05
KellyKiiru <infowithkiiru@gmail.com>
2023-02-13 22:40:41 +0300

    comment out redundant code

--- thedrop/urls.py
+++ thedrop/urls.py
@@ -15,14 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/clearcache/', include('clearcache.urls')),
     path('admin/', admin.site.urls),
     path('',include('instergram.urls')),
+    # path('accounts/', include('allauth.urls')),
+    # path('login/', auth_views.LoginView.as_view(),name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(),name='logout'),
     path('accounts/',include('registration.backends.simple.urls')),
-    #path('login/', auth_views.LoginView.as_view(),name='login'),
-    #path('logout/', auth_views.LogoutView.as_view(),name='logout'),
 ]
