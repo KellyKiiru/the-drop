b5383f5eb9359535b119630104a01a4ad178d8d8
KellyKiiru <infowithkiiru@gmail.com>
2023-09-01 00:28:22 +0300

    changes to homepage url path string

--- instergram/urls.py
+++ instergram/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns= [
-    path('',views.homepage,name='homepage'),
+    path('homepage/',views.homepage,name='homepage'),
     # path('signup',views.signup,name='sign-up'),
     path('login/', auth_views.LoginView.as_view(),name='login'),
     path('newpost', views.newpost, name='newpost'),
@@ -14,7 +14,7 @@ urlpatterns= [
     path('accounts/<int:id>/', views.userprofile, name='profile'),
     path('profile/edit', views.editprofile, name="editprofile"),
     #path('sign-in/', auth_views.LoginView.as_view(template_name="registration/sign-in.html", redirect_authenticated_user=True), name='sign-in'),
-    path('logout/',auth_views.LoginView.as_view(),{"next_page": '/'},name='logout'),
+    # path('logout/',auth_views.LoginView.as_view(),{"next_page": '/'},name='logout'),
     path('<username>/follow/<option>/', follow, name='follow'),
 ]
 if settings.DEBUG:
