bf09832a59b9c8691c01adc210a65bca895a716b
kellykiiru <infowithkiiru@gmail.com>
2024-01-01 21:49:57 +0300

    delete

--- thedrop/settings.py
+++ thedrop/settings.py
@@ -4,7 +4,6 @@ from decouple import config
 from dotenv import load_dotenv
 
 load_dotenv()
-#from database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
